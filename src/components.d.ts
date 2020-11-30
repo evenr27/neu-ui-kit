d6206479a3d9c480f199a7de7ddd6f569ea7b97f
evenr27 <evenr27@gmail.com>
2020-11-29 20:27:18 -0500

    feat(neu-button):testing styles

--- src/components.d.ts
+++ src/components.d.ts
@@ -6,30 +6,10 @@
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
 export namespace Components {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first": string;
-        /**
-          * The last name
-         */
-        "last": string;
-        /**
-          * The middle name
-         */
-        "middle": string;
-    }
     interface NeuButton {
     }
 }
 declare global {
-    interface HTMLMyComponentElement extends Components.MyComponent, HTMLStencilElement {
-    }
-    var HTMLMyComponentElement: {
-        prototype: HTMLMyComponentElement;
-        new (): HTMLMyComponentElement;
-    };
     interface HTMLNeuButtonElement extends Components.NeuButton, HTMLStencilElement {
     }
     var HTMLNeuButtonElement: {
@@ -37,29 +17,13 @@ declare global {
         new (): HTMLNeuButtonElement;
     };
     interface HTMLElementTagNameMap {
-        "my-component": HTMLMyComponentElement;
         "neu-button": HTMLNeuButtonElement;
     }
 }
 declare namespace LocalJSX {
-    interface MyComponent {
-        /**
-          * The first name
-         */
-        "first"?: string;
-        /**
-          * The last name
-         */
-        "last"?: string;
-        /**
-          * The middle name
-         */
-        "middle"?: string;
-    }
     interface NeuButton {
     }
     interface IntrinsicElements {
-        "my-component": MyComponent;
         "neu-button": NeuButton;
     }
 }
@@ -67,7 +31,6 @@ export { LocalJSX as JSX };
 declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
-            "my-component": LocalJSX.MyComponent & JSXBase.HTMLAttributes<HTMLMyComponentElement>;
             "neu-button": LocalJSX.NeuButton & JSXBase.HTMLAttributes<HTMLNeuButtonElement>;
         }
     }
